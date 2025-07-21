706dbe559b9bfca3d1311ab06a0755349a1c6b50
Diego Amorim - Dockzn <diego91077@gmail.com>
2025-07-21 10:28:38 -0300

    tela de inicio e header componentizada

---
+++ frontend/components/navbar/navbar.js
@@ -0,0 +1,27 @@
+function criarNavbar() {
+  const navbar = document.createElement('nav');
+  navbar.classList.add('navbar');
+
+  navbar.innerHTML = `
+    <div class="navbar-container">
+      <div class="navbar-logo">
+        <img src="../../assets/images/Logo Unigroup.png" alt="Logo Unigroup">
+      </div>
+      <div class="navbar-links">
+        <input type="text" placeholder="Pesquisar projeto ou equipe" class="search-input">
+        <button class="button-newproject">Criar novo projeto</button>
+      </div>
+      <div class="navbar-usuario">
+        <button class="button-user"><i class="fa-solid fa-circle-user"></i></button>
+      </div>
+    </div>
+  `;
+
+  return navbar;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+  const navbar = criarNavbar();
+  header.appendChild(navbar);
+});
